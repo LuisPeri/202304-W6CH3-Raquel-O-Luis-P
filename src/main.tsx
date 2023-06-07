2f678040f70a43f713f08abc9fcdde7f50aa15d6
Luis <luisperi18@hotmail.com>
2023-06-07 18:08:00 +0200

    Add store file

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./got/components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./styles.css";
 import { Provider } from "react-redux";
+import { store } from "./core/store/store";
 
 ReactDOM.createRoot(document.querySelector(".container") as HTMLElement).render(
   <React.StrictMode>
